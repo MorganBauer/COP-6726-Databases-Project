87f5c152572b499e7d831d58f47804a6fbf83d01
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-01-22 16:32:53 -0500

    Comment relation data members.

--- test.h
+++ test.h
@@ -8,72 +8,72 @@
 using namespace std;
 
 extern "C" {
-	int yyparse(void);   // defined in y.tab.c
+  int yyparse(void);   // defined in y.tab.c
 }
 
 extern struct AndList *final;
 
 class Relation {
 
-private:
-	char *rname;
-	char *prefix;
-	char rpath[100]; 
-	Schema *rschema;
-public:
-	Relation (char *_name, Schema *_schema, char *_prefix) :
-		rname (_name), rschema (_schema), prefix (_prefix) {
-		sprintf (rpath, "%s%s.bin", prefix, rname);
-	}
-	char* name () { return rname; }
-	char* path () { return rpath; }
-	Schema* schema () { return rschema;}
-	void info () {
-		cout << " Relation info\n";
-		cout << "\t name: " << name () << endl;
-		cout << "\t path: " << path () << endl;
-	}
+ private:
+  char *rname; // Relation name
+  char *prefix; //
+  char rpath[100]; // path to relation
+  Schema *rschema;
+ public:
+  Relation (char *_name, Schema *_schema, char *_prefix) :
+  rname (_name), rschema (_schema), prefix (_prefix) {
+    sprintf (rpath, "%s%s.bin", prefix, rname);
+  }
+  char* name () { return rname; }
+  char* path () { return rpath; }
+  Schema* schema () { return rschema;}
+  void info () {
+    cout << " Relation info\n";
+    cout << "\t name: " << name () << endl;
+    cout << "\t path: " << path () << endl;
+  }
 
-	void get_cnf (CNF &cnf_pred, Record &literal) {
-		cout << " Enter CNF predicate (when done press ctrl-D):\n\t";
-  		if (yyparse() != 0) {
-			std::cout << "Can't parse your CNF.\n";
-			exit (1);
-		}
-		cnf_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
-	}
+  void get_cnf (CNF &cnf_pred, Record &literal) {
+    cout << " Enter CNF predicate (when done press ctrl-D):\n\t";
+    if (yyparse() != 0) {
+      std::cout << "Can't parse your CNF.\n";
+      exit (1);
+    }
+    cnf_pred.GrowFromParseTree (final, schema (), literal); // constructs CNF predicate
+  }
 };
 
-char *supplier = "supplier"; 
-char *partsupp = "partsupp"; 
-char *part = "part"; 
-char *nation = "nation"; 
-char *customer = "customer"; 
-char *orders = "orders"; 
-char *region = "region"; 
-char *lineitem = "lineitem"; 
+char *supplier = "supplier";
+char *partsupp = "partsupp";
+char *part = "part";
+char *nation = "nation";
+char *customer = "customer";
+char *orders = "orders";
+char *region = "region";
+char *lineitem = "lineitem";
 
 Relation *s, *p, *ps, *n, *li, *r, *o, *c;
 
 void setup (char *catalog_path, char *dbfile_dir, char *tpch_dir) {
-	cout << " \n** IMPORTANT: MAKE SURE THE INFORMATION BELOW IS CORRECT **\n";
-	cout << " catalog location: \t" << catalog_path << endl;
-	cout << " tpch files dir: \t" << tpch_dir << endl;
-	cout << " heap files dir: \t" << dbfile_dir << endl;
-	cout << " \n\n";
+  cout << " \n** IMPORTANT: MAKE SURE THE INFORMATION BELOW IS CORRECT **\n";
+  cout << " catalog location: \t" << catalog_path << endl;
+  cout << " tpch files dir: \t" << tpch_dir << endl;
+  cout << " heap files dir: \t" << dbfile_dir << endl;
+  cout << " \n\n";
 
-	s = new Relation (supplier, new Schema (catalog_path, supplier), dbfile_dir);
-	ps = new Relation (partsupp, new Schema (catalog_path, partsupp), dbfile_dir);
-	p = new Relation (part, new Schema (catalog_path, part), dbfile_dir);
-	n = new Relation (nation, new Schema (catalog_path, nation), dbfile_dir);
-	li = new Relation (lineitem, new Schema (catalog_path, lineitem), dbfile_dir);
-	r = new Relation (region, new Schema (catalog_path, region), dbfile_dir);
-	o = new Relation (orders, new Schema (catalog_path, orders), dbfile_dir);
-	c = new Relation (customer, new Schema (catalog_path, customer), dbfile_dir);
+  s = new Relation (supplier, new Schema (catalog_path, supplier), dbfile_dir);
+  ps = new Relation (partsupp, new Schema (catalog_path, partsupp), dbfile_dir);
+  p = new Relation (part, new Schema (catalog_path, part), dbfile_dir);
+  n = new Relation (nation, new Schema (catalog_path, nation), dbfile_dir);
+  li = new Relation (lineitem, new Schema (catalog_path, lineitem), dbfile_dir);
+  r = new Relation (region, new Schema (catalog_path, region), dbfile_dir);
+  o = new Relation (orders, new Schema (catalog_path, orders), dbfile_dir);
+  c = new Relation (customer, new Schema (catalog_path, customer), dbfile_dir);
 }
 
 void cleanup () {
-	delete s, p, ps, n, li, r, o, c;
+  delete s, p, ps, n, li, r, o, c;
 }
 
 #endif
