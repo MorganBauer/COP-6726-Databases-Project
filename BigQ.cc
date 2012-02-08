02aa60de6eb12a8d06e115225c7f636e94b58197
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-02-07 21:07:02 -0500

    simplest thing to get input to output, nothing else

--- BigQ.cc
+++ BigQ.cc
@@ -1,13 +1,19 @@
 #include "BigQ.h"
 
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen) {
-	// read data from in pipe sort them into runlen pages
+  // read data from in pipe sort them into runlen pages
 
-    // construct priority queue over sorted runs and dump sorted data 
- 	// into the out pipe
+  // proof of concept, simplest thing that could possibly work
+  // for completely broken values of work
+  Record temp;
+  while (in.Remove(&temp))
+    out.Insert(&temp);
 
-    // finally shut down the out pipe
-	out.ShutDown ();
+  // construct priority queue over sorted runs and dump sorted data
+  // into the out pipe
+
+  // finally shut down the out pipe
+  out.ShutDown ();
 }
 
 BigQ::~BigQ () {
