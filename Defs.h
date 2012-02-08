d382e67849c553bf5f98547f6587abd1ef70c7f0
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-02-07 20:10:50 -0500

    experimental likely unlikely macros

--- Defs.h
+++ Defs.h
@@ -18,3 +18,10 @@ unsigned int Random_Generate();
 
 #endif
 
+#ifdef __GNUC__
+#define likely(x)       __builtin_expect((x),1)
+#define unlikely(x)     __builtin_expect((x),0)
+#else
+#define likely(x)       (x)
+#define unlikely(x)     (x)
+#endif
