31a6b5b4f205be11f900030883eef9e7d45c5cb7
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-02-20 02:47:04 -0500

    test 1 of test2-1 should work

--- BigQ.cc
+++ BigQ.cc
@@ -1,17 +1,17 @@
 #include "BigQ.h"
 // #include <boost/thread.hpp>
 #include <vector>
+#include <queue>
 #include <cstdlib>
 #include <cstdio>
 #include <iostream>
 #include <utility>
 #include <algorithm>
 #include <cassert>
-
+#include <iterator>
 BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen)
-  : in(in),out(out),sortorder(sortorder),runlen(runlen), pagesInserted(0)
+  : in(in),out(out),sortorder(sortorder),runlen(runlen), pagesInserted(0), runCount(0), totalRecords(0)
 {
-
   pthread_create (&worker_thread, NULL, &BigQ::thread_starter, this);
 }
 
@@ -28,7 +28,7 @@ void * BigQ :: WorkerThread(void) {
   // FIRST PHASE
   PhaseOne();
   // in pipe should be dead now.
-
+  cout << totalRecords << " Records written to file" << endl;
   // SECOND PHASE
   PhaseTwo();
   cout << "cleanup" << endl;
@@ -128,6 +128,7 @@ void BigQ :: writeSortedRunToFile(vector<Record> & runlenrecords)
   for (vector<Record>::iterator it = runlenrecords.begin(); it < runlenrecords.end(); it++)
     {
       Record trp;
+      totalRecords++;
       trp.Consume(&(*it));
       if(0 == tp.Append(&trp))
         {
@@ -145,20 +146,143 @@ void BigQ :: writeSortedRunToFile(vector<Record> & runlenrecords)
 
 void BigQ::PhaseTwo(void)
 {
-  cout << "merging sorted runs" << endl; 
+  cout << endl << endl << "merging sorted runs" << endl;
+  cout << runCount << " runs in " << partiallySortedFile.GetLength() << " total pages" << endl;
+  for (std::vector < std::pair <off_t,off_t> >::iterator it = runLocations.begin(); it < runLocations.end(); it++)
+    {
+      cout << "from " << (*it).first << " to " << (*it).second << endl;
+    }
+
+  { // Linear scan of minimums
+    // initialize page containers
+    /*{// alternate version of manual page management
+      vector<vector<Record > > pages;
+      for (int i; i < runCount; i++)
+      {
+      Page tp;
+      Record tr;
+
+      }
+      }*/
+
+    // Run(1,runLocations[1].first,runLocations[1].second, & partiallySortedFile);
+
+
+
+
+    vector<Run> runs;
+    runs.reserve(runCount);
+    cout << "initializing runs" << endl;
+    for (int i = 0; i < runCount; i++)
+      {
+        cout << "Run " << i;
+        runs.push_back(Run(i,runLocations[i].first,runLocations[i].second, &partiallySortedFile));
+        cout << " initialized" << endl;
+        runs[i].print();
+      }
+
+    for (int i = 0; i < runCount; i++)
+      {
+        runs[i].print();
+      }
+    
+    vector<Record> minimums;
+    // initialize minimums
+    // for each run, get the first guy.
+    cout << "initializing minimums" << endl;
+    minimums.reserve(runCount);
+    for (int i = 0; i < runCount; i++)
+      {
+        cout << "minimum " << i;
+        Record tr;
+        runs[i].getNextRecord(tr);
+        minimums.push_back(tr);
+        cout << "initialized " << endl;
+      }
+    // now find the minimum guy and put it in the pipe
+    // do this totalRecords times
+    cout << "putting stuff in the pipe" << endl;
+    Compare c = Compare(sortorder);
+    int runsLeft = runCount;
+    for (int r = totalRecords ; r > 0; r--)
+      {
+        vector<Record>::iterator min = std::min_element(minimums.begin(), minimums.end(),Compare(sortorder));//Compare(sortorder));
+        vector<Record>::iterator::difference_type run = std::distance( minimums.begin(), min);
+        // cout << "record from run " << ((int)run) << " was chosen" << endl;
+
+        std::vector<Record>::iterator result = std::min_element(minimums.begin(), minimums.end(),Compare(sortorder));
+        // std::cout << "min element at: " << std::distance(minimums.begin(), result) << endl ;
+        long int d = std::distance(minimums.begin(), result) ;
+        vector<Record>::iterator::difference_type d2 = std::distance(minimums.begin(), result) ;
+        // cout << d << "&"<< d2 << endl;
+        
+        Record tr;
+        tr.Consume(&(minimums[run]));
+        out.Insert(&tr);
+        bool valid = runs[run].getNextRecord(tr);
+        if (valid)
+          {
+            minimums[run].Consume(&tr);
+          }
+        else
+          {
+            cout << "run empty, got to get rid of it" << endl;
+            runsLeft--;
+            minimums.erase(minimums.begin() + run);
+            runs.erase(runs.begin() + run);
+            // need to get rid of run and shift everything over
+          }
+      }
+    cout << minimums.size() << runs.size() << endl;
+    cout << "runs left = "<< runsLeft << endl;
+    assert (0 == runsLeft);
+
+  }
+
   // construct priority queue over sorted runs and dump sorted data
   // into the out pipe
+
+  // Page tp;
+  // Record tr;
+
+  // partiallySortedFile.GetPage(&tp,0);
+  // tp.GetFirst(&tr);
+  // priority_queue<Record, vector<Record>, Compare > prioQueueOverRuns (Compare(sortorder));
+  // prioQueueOverRuns.push(tp);
+
+  // get first page of each run, put into extra vector
+  // git first record of each page, put into queue
+
+  // while (not empty)/(not finished)
+  // pull out minimum
+  // put into pipe.
+
+  /*
+    { // priority queue stuff, figure out later, if even worthwhile.
+    Record temp;
+    PQ pq(runCount, sortorder);
+    while( !pq.Empty() )
+    {
+    Record temp;
+    pq.getMinimum(temp);
+    out.Insert(&temp);
+    }
+    }
+  */
 
   // for each record we need from a specific page
   // read specific page
   // get record from page
   // write modified page back
+  ReadModifyWrite();
 
   /* OR */
 
   // read page into buffer of pages, when it is empty, get next page from that sequence if it is available, and write that out.
 
   // iterate through pages putting them all in the pipe directly
+  /*
+  int totalRecordsOut = 0;
   off_t lastPage = partiallySortedFile.GetLength() - 1;
   for(off_t curPage = 0;  curPage < lastPage; curPage++)
     {
@@ -167,11 +291,22 @@ void BigQ::PhaseTwo(void)
       Record temp;
       while(1 == tp.GetFirst(&temp))
         {
+          totalRecordsOut++;
           out.Insert(&temp);
           // cout << "put a record in the pipe" << endl;
         }
     }
+  cout << totalRecordsOut << " Records written to pipe" << endl;
+  assert(totalRecordsOut == totalRecords);
+  */
+  cout << runCount << " runs in " << partiallySortedFile.GetLength() << " total pages" << endl;
+  cout << "runlen of " << runlen << endl;
   cout << "phase two complete" << endl;
+}
+
+void BigQ::ReadModifyWrite(void)
+{
+  cout << "RMW" << endl;
 }
 
 BigQ::~BigQ () {
--- BigQ.h
+++ BigQ.h
@@ -1,14 +1,111 @@
 #ifndef BIGQ_H
 #define BIGQ_H
 #include <pthread.h>
+#include <iostream>
 #include <vector>
 #include <utility>
 #include "Pipe.h"
 #include "File.h"
 #include "Record.h"
 #include <functional>
+#include <cstdlib>
 
 using namespace std;
+
+class Run
+{
+ private:
+  int runID;
+  off_t start_offset;
+  off_t end_offset;
+  off_t cur_offset;
+  Page p;
+  Record r;
+  bool empty;
+  File * partiallySortedFile;
+  /* class CompareMin : public std::binary_function <Record &, Record &, bool> */
+  /*   { */
+  /*   private: */
+  /*     OrderMaker & so; */
+  /*   public: */
+  /*   CompareMin(OrderMaker so) :so(so){} */
+  /*     bool operator()(Record & x, Record & y) */
+  /*     { */
+  /*       ComparisonEngine comp; */
+  /*       return  (comp.Compare((&x), (&y), &so) < 0); */
+  /*     } */
+  /*   } cm; */
+  void getNextPage(Page & p,int i)
+  {
+  }
+ public:
+  // Run(int rID, off_t start, off_t end, OrderMaker & sortorder): runID(rID), start_offset(start), end_offset(end), cm(sortorder) { empty = false;}
+ Run(int rID, off_t start, off_t end, File * partiallySortedFile):
+  runID(rID), start_offset(start), end_offset(end), cur_offset(start), empty(false)
+    { this->partiallySortedFile = partiallySortedFile;
+      partiallySortedFile->GetPage(&p,start_offset);
+      cout << "Run constructor called" << partiallySortedFile->GetLength() << endl;}
+  Run(const Run & rn)
+    {
+      cout << "copy constructor called" << endl;
+      runID = rn.runID;
+      start_offset = rn.start_offset;
+      end_offset = rn.end_offset;
+      cur_offset = rn.cur_offset;
+      empty = rn.empty;
+      // rn.partiallySortedFile->AddPage(&rn.p,rn.cur_offset);
+      rn.partiallySortedFile->AddPage(const_cast<Page *>(&rn.p),cur_offset); // violating the const promise so hard right now
+      partiallySortedFile = rn.partiallySortedFile;
+      partiallySortedFile->GetPage(&p,cur_offset);
+    }
+  Run & operator= (const Run & rn)
+    {
+      cout << "assignment operator called" << endl;
+      runID = rn.runID;
+      start_offset = rn.start_offset;
+      end_offset = rn.end_offset;
+      cur_offset = rn.cur_offset;
+      empty = rn.empty;
+      rn.partiallySortedFile->AddPage(const_cast<Page *>(&rn.p),cur_offset); // violating the const promise so hard right now
+      partiallySortedFile = rn.partiallySortedFile;
+      partiallySortedFile->GetPage(&p,cur_offset);
+    }
+  ~Run ()
+    {cout << "Run destructor called" << endl;
+      cout << "destroyed run " << runID << endl;}
+  // returns true if a page was returned.
+  bool getNextRecord(Record & ret)
+  {
+    // cout << "R.GNR of " << runID << endl;
+    if( 1 == p.GetFirst(&r) )
+      { // page had something in it, have a record now.
+        ret.Consume(&r);
+        return true;
+      }
+    else
+      { // page was empty, get new page
+        cur_offset++;
+        cout << cur_offset << endl;
+        // check if out of bounds
+        if (cur_offset < end_offset)
+          {
+            partiallySortedFile->GetPage(&p,cur_offset);
+            if( 1 == p.GetFirst(&r) )
+              { // page had something in it, have a record now.
+                ret.Consume(&r);
+                return true;
+              }
+            else{ // error
+              exit(-1);}
+          }
+        else{ return false;}
+      }
+  }
+  void print()
+  {std::cout << "Run " << runID << ", starting at " << start_offset << " and running to " << end_offset << endl;}
+  // bool operator< (Run & rhs)  { return empty ? false : cm(r,rhs.r);}
+};
+
 
 class BigQ {
  private:
@@ -20,6 +117,7 @@ class BigQ {
   File partiallySortedFile;
   std::vector <pair <off_t, off_t> > runLocations;
   int pagesInserted;
+  int totalRecords;
   int runCount;
 
   pthread_t worker_thread;
@@ -32,6 +130,7 @@ class BigQ {
   void writeSortedRunToFile(std::vector<Record> & runlenrecords);
 
   void PhaseTwo(void);
+  void ReadModifyWrite(void);
 
   class Compare : public std::binary_function <Record &, Record &, bool>
     {
@@ -43,11 +142,53 @@ class BigQ {
         return  (comp.Compare(const_cast<Record *>(&x), const_cast<Record *>(&y), &so) < 0); }
     };
 
+  class PQ
+  {
+    bool empty;
+    vector<Run> runs;
+    class CompareMin : public std::binary_function <Record &, Record &, bool>
+      {
+      private:
+        OrderMaker & so;
+      public:
+      CompareMin(OrderMaker so) :so(so){}
+        bool operator()(const Record & x, const Record & y)
+        {
+          ComparisonEngine comp;
+          //if (x.bitsNull
+          return  (comp.Compare(const_cast<Record *>(&x), const_cast<Record *>(&y), &so) < 0);
+        }
+      } cm;
+
+  public:
+  PQ(int runCount, OrderMaker & sortorder): cm(sortorder) { empty = false; runs.reserve(runCount);}
+    bool Empty(void)
+    {return empty;}
+    void getMinimum(Record & r)
+    {
+      for (vector<Run>::iterator it = runs.begin(); it < runs.end() - 1; it++)
+        {
+          // compare it,it+1
+        }
+    }
+  };
+
+  class TaggedRecord
+  {
+  private:
+    int run;
+    Record r;
+  public:
+
+  };
+
  public:
   BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runlen);
   ~BigQ ();
 
 
 };
+
+
 
 #endif
