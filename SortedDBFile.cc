afadf37d9c8bc8dfa9e791ca46c7ab08756d953b
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-02-25 23:12:11 -0500

    method stubs for implementation of 2-2

--- SortedDBFile.cc
+++ SortedDBFile.cc
@@ -15,3 +15,38 @@
 
 /* Morgan Bauer */
 
+
+SortedDBFile::SortedDBFile ()
+{}
+
+int SortedDBFile::Open (char *f_path)
+{
+}
+
+int SortedDBFile::Create (char *f_path, fType f_type, void *startup)
+{
+}
+
+void SortedDBFile::Load (Schema &f_schema, char *loadpath)
+{
+}
+
+void SortedDBFile::MoveFirst ()
+{
+}
+
+int SortedDBFile::Close ()
+{
+}
+
+void SortedDBFile::Add (Record &rec)
+{
+}
+
+int SortedDBFile::GetNext (Record &fetchme)
+{
+}
+
+int SortedDBFile::GetNext (Record &fetchme, CNF &cnf, Record &literal)
+{
+}
