63b244847f2dd09ff759c97dd52ac2ea957b5cc0
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-02-25 23:12:00 -0500

    need this to compile.

---
+++ SortedDBFile.cc
@@ -0,0 +1,17 @@
+#include "TwoWayList.h"
+#include "Record.h"
+#include "Schema.h"
+#include "File.h"
+#include "Comparison.h"
+#include "ComparisonEngine.h"
+#include "DBFile.h"
+#include "SortedDBFile.h"
+#include "Defs.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cassert>
+#include <cstdlib>
+
+/* Morgan Bauer */
+
