f95928e22b89fdf1d8feb427a83a9df5ccf74dbd
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-03-05 00:10:28 -0500

    Everything but getnext CNF should work
    have internal string of the path saved so I can close the internal file handle and reopen it, so that I can clear out the file
    the default merge strategy is to put everything into the differential bigq and then wipe the old file and write a new one.

--- SortedDBFile.h
+++ SortedDBFile.h
@@ -7,18 +7,28 @@
 #include "DBFileDefs.h"
 #include "GenericDBFile.h"
 #include "BigQ.h"
+#include <string>
 /* sub class for dbfile impl, supporting sorted operations*/
 
 class SortedDBFile : public GenericDBFile
 {
+ private:
+  enum rw_mode {reading, writing};
+  rw_mode currentRWMode;
+  string filepath;
   File f;
   Page curPage;
   off_t curPageIndex;
 
   int runLength;
   OrderMaker so;
+  static const int pipeBufferSize = 100;
+  Pipe * toBigQ;
+  Pipe * fromBigQ;
   BigQ * bq;
 
+  void MergeDifferential (void);
+  void StupidMergeDifferential (void);
  public:
   SortedDBFile ();
 
