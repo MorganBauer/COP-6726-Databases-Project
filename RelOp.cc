aa4e47e8a482e34addc31f4f7a35e424237b61af
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-03-19 22:12:38 -0400

    added sum, made use_n_pages part of superclass
    sum implemented
    with threading
    not quite right, but not sure why at the moment.

--- RelOp.cc
+++ RelOp.cc
@@ -1,11 +1,15 @@
 #include "RelOp.h"
+#include <iostream>
+#include <sstream>
+#include <string>
 
 void SelectFile::Run (DBFile &inFile_, Pipe &outPipe_, CNF &selOp_, Record &literal_) {
+  clog << "select file starting" << endl;
   inF = &inFile_;
   outP = &outPipe_;
   cnf = &selOp_;
   lit = &literal_;
-  pthread_create (&selectFileThread, NULL, &SelectFile::thread_starter, this);
+  pthread_create (&SelectFileThread, NULL, &SelectFile::thread_starter, this);
 }
 
 void * SelectFile :: thread_starter(void *context)
@@ -14,13 +18,14 @@ void * SelectFile :: thread_starter(void *context)
 }
 
 void * SelectFile :: WorkerThread(void) {
+  clog << "SF worker thread started" << endl;
   DBFile & inFile = *inF;
   Pipe & outPipe = *outP;
   CNF & selOp = *cnf;
   Record & literal = *lit;
   int counter = 0;
   Record temp;
-  while (inFile.GetNext (temp, selOp, literal) == 1) {
+  while (SUCCESS == inFile.GetNext (temp, selOp, literal)) {
     counter += 1;
     if (counter % 10000 == 0) {
       cout << counter << "\n";
@@ -30,14 +35,59 @@ void * SelectFile :: WorkerThread(void) {
   cout << " selected " << counter << " recs \n";
 
   outPipe.ShutDown();
+  clog << "select file ending, after selecting " << counter << " records" << endl;
   pthread_exit(NULL); // make our worker thread go away
 }
 
 void SelectFile::WaitUntilDone () {
-  pthread_join (selectFileThread, NULL);
+  clog << "SF waiting til done" << endl;
+  pthread_join (SelectFileThread, NULL);
+  clog << "SF complete, joined" << endl;
 }
 
-void SelectFile::Use_n_Pages (int runlen) {
-  runLength = runlen;
-  return;
+void * Sum :: thread_starter(void *context)
+{
+  clog << "starting sum thread" << endl;
+  return reinterpret_cast<Sum*>(context)->WorkerThread();
+}
+
+void * Sum :: WorkerThread(void) {
+  cout << "Sum thread started" << endl;
+  Pipe &inPipe = *in;
+  Pipe &outPipe = *out;
+  Function &computeMe = *fn;
+  Record temp;
+  clog << "begin summing" << endl;
+  Type retType;
+  while(SUCCESS == inPipe.Remove(&temp))
+    {
+      retType = computeMe.Apply(temp,integerResult,FPResult);
+    }
+  clog << "summing complete" << endl;
+  // sum complete, take value from function and put into outpipe.
+  {
+    Record ret;
+    stringstream ss;
+    Attribute attr;
+    attr.name = "SUM";
+    if (Int == retType)
+      {
+        clog << "int result" << endl;
+        attr.myType = Int;
+        ss << integerResult;
+        ss << "|";
+      }
+    else if (Double == retType) // floating point result
+      {
+        clog << "double result" << endl;
+        attr.myType = Double;
+        ss << FPResult;
+        ss << "|";
+      }
+    Schema retSchema ("out_schema",1,&attr);
+    ret.ComposeRecord(&retSchema, ss.str().c_str());
+    outPipe.Insert(&ret);
+  }
+  outPipe.ShutDown();
+  pthread_exit(NULL); // make our worker thread go away
 }
--- RelOp.h
+++ RelOp.h
@@ -5,78 +5,100 @@
 #include "DBFile.h"
 #include "Record.h"
 #include "Function.h"
+#include <string>
 
 class RelationalOp {
+  int runLength;
 	public:
 	// blocks the caller until the particular relational operator 
 	// has run to completion
 	virtual void WaitUntilDone () = 0;
 
 	// tell us how much internal memory the operation can use
-	virtual void Use_n_Pages (int n) = 0;
+	void Use_n_Pages (int n) {runLength = n; return;}
 };
 
 class SelectFile : public RelationalOp { 
-
-	private:
-  pthread_t selectFileThread;
+ private:
   DBFile * inF;
   Pipe * outP;
   CNF * cnf;
   Record * lit;
-  
-  int runLength;
 
+  pthread_t SelectFileThread;
   static void *thread_starter(void *context);
   void * WorkerThread(void);
 	public:
 
 	void Run (DBFile &inFile, Pipe &outPipe, CNF &selOp, Record &literal);
 	void WaitUntilDone ();
-	void Use_n_Pages (int n);
-
 };
 
 class SelectPipe : public RelationalOp {
 	public:
 	void Run (Pipe &inPipe, Pipe &outPipe, CNF &selOp, Record &literal) { }
 	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
 };
+
 class Project : public RelationalOp { 
+  int runLength;
+  
+  pthread_t ProjectThread;
+  static void *thread_starter(void *context);
+  void * WorkerThread(void);
 	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) { }
+	void Run (Pipe &inPipe, Pipe &outPipe, int *keepMe, int numAttsInput, int numAttsOutput) {
+          // loop for numattsOut over Keepme
+        }
 	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
 };
+
 class Join : public RelationalOp { 
 	public:
 	void Run (Pipe &inPipeL, Pipe &inPipeR, Pipe &outPipe, CNF &selOp, Record &literal) { }
 	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
 };
+
 class DuplicateRemoval : public RelationalOp {
 	public:
 	void Run (Pipe &inPipe, Pipe &outPipe, Schema &mySchema) { }
 	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
 };
+
 class Sum : public RelationalOp {
+  int runLength;
+  int integerResult;
+  double FPResult;
+
+  Pipe * in;
+  Pipe * out;
+  Function * fn;
+  pthread_t SumThread;
+  static void *thread_starter(void *context);
+  void * WorkerThread(void);
 	public:
-	void Run (Pipe &inPipe, Pipe &outPipe, Function &computeMe) { }
-	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
+ Sum() :runLength(0), integerResult(0),FPResult(0),in(0),out(0),fn(0) {}
+	void Run (Pipe &inPipe, Pipe &outPipe, Function &computeMe) { 
+          in = &inPipe;
+          out = &outPipe;
+          fn = &computeMe;
+          clog << "SUM pthread create" << endl;
+          pthread_create (&SumThread, NULL, &Sum::thread_starter, this);
+        }
+	void WaitUntilDone () {
+          clog << "Sum waiting" << endl;
+          pthread_join (SumThread, NULL);
+          clog << "Sum complete, joined" << endl;
+        }
 };
 class GroupBy : public RelationalOp {
 	public:
 	void Run (Pipe &inPipe, Pipe &outPipe, OrderMaker &groupAtts, Function &computeMe) { }
 	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
 };
 class WriteOut : public RelationalOp {
 	public:
 	void Run (Pipe &inPipe, FILE *outFile, Schema &mySchema) { }
 	void WaitUntilDone () { }
-	void Use_n_Pages (int n) { }
 };
 #endif
