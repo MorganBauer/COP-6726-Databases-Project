0299f509521034fe90b50de2b9cbecd7f062b864
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-03-28 18:19:17 -0400

    proper function destructor

--- Function.h
+++ Function.h
@@ -6,15 +6,15 @@
 #define MAX_DEPTH 100
 
 
-enum ArithOp {PushInt, PushDouble, ToDouble, ToDouble2Down, 
+enum ArithOp {PushInt, PushDouble, ToDouble, ToDouble2Down,
 	IntUnaryMinus, IntMinus, IntPlus, IntDivide, IntMultiply,
 	DblUnaryMinus, DblMinus, DblPlus, DblDivide, DblMultiply};
 
 struct Arithmatic {
-
 	ArithOp myOp;
 	int recInput;
-	void *litInput;	
+	void *litInput;
+Arithmatic() : myOp(ToDouble2Down), recInput(-1), litInput(0) {}
 };
 
 class Function {
@@ -29,7 +29,23 @@ private:
 public:
 
 	Function ();
-
+        ~Function () {
+          for (int i = 0; i < numOps; i++)
+            {
+              if (-1 == opList[i].recInput) // check for literal values and free them.
+                {
+                  if (PushInt == opList[i].myOp)
+                    {
+                      delete (int *) opList[i].litInput;
+                    }
+                  else if (PushDouble == opList[i].myOp)
+                    {
+                      delete (double *)opList[i].litInput;
+                    }
+                }
+            }
+          delete[] opList;
+        };
 	// this grows the specified function from a parse tree and converts
 	// it into an accumulator-based computation over the attributes in
 	// a record with the given schema; the record "literal" is produced
