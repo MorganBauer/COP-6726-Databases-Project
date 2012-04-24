14c5d95534cd99ff4325c98da4e1410648dd69aa
Morgan Harold Bauer <bauer.morgan@gmail.com>
2012-04-23 22:30:37 -0400

    initial changes to understand how to do stuff

--- main.cc
+++ main.cc
@@ -5,23 +5,90 @@
 using namespace std;
 
 extern "C" {
-	int yyparse(void);   // defined in y.tab.c
+  int yyparse(void);   // defined in y.tab.c
+}
+
+void PrintParseTree(struct AndList *pAnd)
+{
+  while (pAnd)
+    {
+      struct OrList *pOr = pAnd->left;
+      while (pOr)
+        {
+          struct ComparisonOp *pCom = pOr->left;
+          if (pCom!=NULL)
+            {
+              {
+                struct Operand *pOperand = pCom->left;
+                if(pOperand!=NULL)
+                  {
+                    cout<<pOperand->value<<" ";
+                  }
+              }
+              switch(pCom->code)
+                {
+                case 1:
+                  cout<<" < "; break;
+                case 2:
+                  cout<<" > "; break;
+                case 3:
+                  cout<<" = ";
+                }
+              {
+                struct Operand *pOperand = pCom->right;
+                if(pOperand!=NULL)
+                  {
+                    cout<<pOperand->value<<" ";
+                  }
+              }
+            }
+          if(pOr->rightOr)
+            {
+              cout<<" OR ";
+            }
+          pOr = pOr->rightOr;
+        }
+      if(pAnd->rightAnd)
+        {
+          cout<<" AND ";
+        }
+      pAnd = pAnd->rightAnd;
+    }
+  cout << endl;
 }
 
 int main () {
 
 
 
-	yyparse();
+  yyparse();
 
-// these data structures hold the result of the parsing
-struct FuncOperator *finalFunction;
-struct TableList *tables;
-struct AndList *boolean;
-struct NameList *groupingAtts;
-struct NameList *attsToSelect;
-int distinctAtts;
-int distinctFunc;
+  // these data structures hold the result of the parsing
+  extern struct FuncOperator *finalFunction;
+  extern struct TableList *tables;
+  extern struct AndList *boolean;
+  extern struct NameList *groupingAtts;
+  extern struct NameList *attsToSelect;
+  extern int distinctAtts;
+  extern int distinctFunc;
+
+
+  PrintParseTree(boolean);
+  // SELECT SUM DISTINCT (a.b + b), d.g
+  // FROM a AS b
+  // WHERE ('foo' > this.that OR 2 = 3) AND (12 > 5)
+  // GROUP BY a.f, c.d, g.f
+
+  // q6 first ish., pure select.
+  // q3 looks pretty easy
+  // q10
+  // q4 as well, superfluous join,
+  // selects before joins.
+
+  clog << "distinct atts " << distinctAtts << endl;
+  clog << "function for distinct was :" << distinctFunc << ". 0 means SUM, 1 means SUM DISTINCT" << endl;
+
+
 }
 
 
